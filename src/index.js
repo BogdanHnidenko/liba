8bcaacea574d2567f20de122b6952ee8f88bfc0e
bogdan_hnidenko <bodyaghnidenko@gmail.com>
2024-07-26 15:32:01 +0300

    wt345fg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // ----------------- Модуль для передачі функції ------------------
-const labelPrintData = require('./labelPrintData'); // Модуль перевірки наявності обʼєктів та отримання данних
+const labelPrintData = require('./labelPrintData.js'); // Модуль перевірки наявності обʼєктів та отримання данних
 
 module.exports = {
   labelPrintData
