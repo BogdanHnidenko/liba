e9f17f0d60da55467c068b519bd895f767f061ea
bogdan_hnidenko <bodyaghnidenko@gmail.com>
2024-07-26 13:21:14 +0300

    Додав перевірки різні

--- index.js
+++
@@ -1,6 +0,0 @@
-// ----------------- Модуль для передачі функції ------------------
-const labelPrintData = require('./src/labelPrintData.js'); // Модуль перевірки наявності обʼєктів та отримання данних
-
-module.exports = {
-  labelPrintData
-};
